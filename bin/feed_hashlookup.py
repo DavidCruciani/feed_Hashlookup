9457698db0ded74336ab627665d946e394d2dda5
David Cruciani <da.cruciani@laposte.net>
2022-06-13 16:11:47 +0200

    chg: [feed] extract file from vm

--- bin/feed_hashlookup.py
+++ bin/feed_hashlookup.py
@@ -41,111 +41,114 @@ if __name__ == '__main__':
     for file in os.listdir(allVariables.pathToWindowsVM):
         vmPath = os.path.join(allVariables.pathToWindowsVM, file)
         vmName = file.split(".")[0]
-        print(f"VM: {vmName}")
 
-        exportPath = os.path.join(allVariables.pathToFeedHashlookup, vmName)
-        if not os.path.isdir(exportPath):
-            os.mkdir(exportPath)
+        if not os.path.isdir(vmPath):
 
-        ## Convert windows machine into raw format
-        convert_file = "%s%s.img" %(allVariables.pathToConvert, vmName)
+            print(f"VM: {vmName}")
 
-        print("## Convertion ##")
-        res = subprocess.call([allVariables.VBoxManage, "clonehd", vmPath, convert_file, "--format", "raw"])
-        print("## Convertion Finish ##\n")
+            exportPath = os.path.join(allVariables.pathToFeedHashlookup, vmName)
+            if not os.path.isdir(exportPath):
+                os.mkdir(exportPath)
 
-        ## create mount directory
-        pathMnt = "./mnt_convert"
-        if not os.path.isdir(pathMnt):
-            os.mkdir(pathMnt)
+            ## Convert windows machine into raw format
+            convert_file = "%s%s.img" %(allVariables.pathToConvert, vmName)
+
+            print("## Convertion ##")
+            res = subprocess.call([allVariables.VBoxManage, "clonehd", vmPath, convert_file, "--format", "raw"])
+            print("## Convertion Finish ##\n")
+
+            ## create mount directory
+            pathMnt = "./mnt_convert"
+            if not os.path.isdir(pathMnt):
+                os.mkdir(pathMnt)
 
 
-        print("[+] Feed Hashlookup setup")
-        
-        ## mount the convert image
-        print("\t[+] Mount")
-        request = "sudo mount -o loop,ro,noexec,noload,offset=$((512*104448)) %s %s" % (convert_file, pathMnt)
-        callSubprocessPopen(request, True)
+            print("[+] Feed Hashlookup setup")
+            
+            ## mount the convert image
+            print("\t[+] Mount")
+            request = "sudo mount -o loop,ro,noexec,noload,offset=$((512*104448)) %s %s" % (convert_file, pathMnt)
+            callSubprocessPopen(request, True)
 
-        intermediate_file = "./intermediate_file" 
-        print("\t[+] List of all files")
-        request = "find %s -type f > %s" % (pathMnt, intermediate_file)
-        callSubprocessPopen(request, True)
+            intermediate_file = "./intermediate_file" 
+            print("\t[+] List of all files")
+            request = "find %s -type f > %s" % (pathMnt, intermediate_file)
+            callSubprocessPopen(request, True)
 
-        data = list()
-        sysinfofile = open(allVariables.pathToSysInfo, "r")
-        sysinfo = sysinfofile.readlines()
-        sysinfofile.close()
-        SysVersion = sysinfo[0].rstrip("\n")
-        SysName = sysinfo[1].rstrip("\n")
-        
-        with open(intermediate_file, 'r') as read_file:
-            for line in read_file.readlines():
-                filename = os.path.normpath(line.rstrip("\n"))
+            data = list()
+            sysinfofile = open(allVariables.pathToSysInfo, "r")
+            sysinfo = sysinfofile.readlines()
+            sysinfofile.close()
+            SysVersion = sysinfo[0].rstrip("\n")
+            SysName = sysinfo[1].rstrip("\n")
+            
+            with open(intermediate_file, 'r') as read_file:
+                for line in read_file.readlines():
+                    filename = os.path.normpath(line.rstrip("\n"))
 
-                if not os.path.isdir(filename):
-                    try:                                
-                        md5Glob = hashlib.md5(open(filename, 'rb').read()).hexdigest()
-                        sha1Glob = hashlib.sha1(open(filename, 'rb').read()).hexdigest()
-                        sha256Glob = hashlib.sha256(open(filename, 'rb').read()).hexdigest()
-                        sha512Glob = hashlib.sha512(open(filename, 'rb').read()).hexdigest()
-                        tlshGlob = tlsh.hash(open(filename, 'rb').read())
-                        ssdeepGlob = ssdeep.hash(open(filename, 'rb').read())
+                    if not os.path.isdir(filename):
+                        try:                                
+                            md5Glob = hashlib.md5(open(filename, 'rb').read()).hexdigest()
+                            sha1Glob = hashlib.sha1(open(filename, 'rb').read()).hexdigest()
+                            sha256Glob = hashlib.sha256(open(filename, 'rb').read()).hexdigest()
+                            sha512Glob = hashlib.sha512(open(filename, 'rb').read()).hexdigest()
+                            tlshGlob = tlsh.hash(open(filename, 'rb').read())
+                            ssdeepGlob = ssdeep.hash(open(filename, 'rb').read())
 
-                        l = line.split("/")
-                        nameFile = ""
-                        for inter in range(2,len(l)):
-                            nameFile += l[inter] + "/"
-                        nameFile = nameFile[:-1]
+                            l = line.split("/")
+                            nameFile = ""
+                            for inter in range(2,len(l)):
+                                nameFile += l[inter] + "/"
+                            nameFile = nameFile[:-1]
 
-                        data.append(
-                            {
-                                'FileName': nameFile.rstrip("\n"),
-                                'FileSize': str(os.path.getsize(filename)),
-                                'Windows:Version': SysVersion,
-                                'Windows:OS': SysName,
-                                'md5': md5Glob,
-                                'sha-1': sha1Glob,
-                                'sha-256': sha256Glob,
-                                'sha-512': sha512Glob,
-                                'tlsh': tlshGlob,
-                                'ssdeep': ssdeepGlob
-                            }
-                        )
+                            data.append(
+                                {
+                                    'FileName': nameFile.rstrip("\n"),
+                                    'FileSize': str(os.path.getsize(filename)),
+                                    'Windows:Version': SysVersion,
+                                    'Windows:OS': SysName,
+                                    'md5': md5Glob,
+                                    'sha-1': sha1Glob,
+                                    'sha-256': sha256Glob,
+                                    'sha-512': sha512Glob,
+                                    'tlsh': tlshGlob,
+                                    'ssdeep': ssdeepGlob
+                                }
+                            )
 
-                        list_sha = list()
-                        i = 0
-                        while i != 6:
-                            list_sha.append(sha1Glob[i] + sha1Glob[i+1])
-                            i += 2
+                            list_sha = list()
+                            i = 0
+                            while i != 6:
+                                list_sha.append(sha1Glob[i] + sha1Glob[i+1])
+                                i += 2
 
-                        path = os.path.join(exportPath, "export")
-                        if not os.path.isdir(path):
-                            os.mkdir(path)
-                        for element in list_sha:
-                            path = os.path.join(path, element)
+                            path = os.path.join(exportPath, "export")
                             if not os.path.isdir(path):
                                 os.mkdir(path)
-                        shutil.copy(filename, path)
-                    except OSError as err:
-                        #print(err)
-                        pass
+                            for element in list_sha:
+                                path = os.path.join(path, element)
+                                if not os.path.isdir(path):
+                                    os.mkdir(path)
+                            shutil.copy(filename, path)
+                        except OSError as err:
+                            #print(err)
+                            pass
 
-        with open(exportPath + "/" + vmName + ".json", 'w') as outfile:
-            ndjson.dump(data, outfile)
+            with open(exportPath + "/" + vmName + ".json", 'w') as outfile:
+                ndjson.dump(data, outfile)
 
-        os.remove(intermediate_file)
+            os.remove(intermediate_file)
 
-        ## umount the convert image
-        print("\t[+] Umount")
-        request = "sudo umount " + pathMnt
-        callSubprocessPopen(request, True)
+            ## umount the convert image
+            print("\t[+] Umount")
+            request = "sudo umount " + pathMnt
+            callSubprocessPopen(request, True)
 
-            
-        ## Suppression of the current raw disk
-        os.remove(convert_file)
-        request = [allVariables.VBoxManage, "closemedium", "disk", convert_file, "--delete"]
-        callSubprocessPopen(request)
+                
+            ## Suppression of the current raw disk
+            os.remove(convert_file)
+            request = [allVariables.VBoxManage, "closemedium", "disk", convert_file, "--delete"]
+            callSubprocessPopen(request)
 
     ## Suppression of mount folder
     try:
